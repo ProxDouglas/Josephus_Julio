dccb3cbcf9a6109df05f4474d2582a467e7911e3
TromeBr <44294126+TromeBr@users.noreply.github.com>
2019-05-28 08:37:11 -0300

    Add files via upload

---
+++ JosephusGuiTeste/Aplicacao.java
@@ -0,0 +1,12 @@
+ 
+public class Aplicacao
+{
+    public static void main(String args[]) {
+    	// Cria GUI para Josephus (qtdeSoldados, intervalo, tempoEspera)
+        JosephusGuiTeste jsfg = new JosephusGuiTeste(50, 2, 300); 
+        
+        // mostra GUI
+        jsfg.mostrarGui(); 
+    }
+}
+ 
